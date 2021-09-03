f56501ce3611068bef44d64f266f30c4e05da14d
akshayaprishap <akshayaprishap@example.com>
2021-09-03 11:04:12 +0530

    updated-features

--- loan_management/loan_management/doctype/loan_details1/loan_details1.py
+++ loan_management/loan_management/doctype/loan_details1/loan_details1.py
@@ -5,37 +5,111 @@ import frappe
 from frappe.model.document import Document
 
 class loandetails1(Document):
-	def on_submit(self):
-		loan_amount= self.loan_amount
-		max_loan_amount=1000000
-		min_loan_amount=10000
-		if(loan_amount >= min_loan_amount and loan_amount <= max_loan_amount):
+	def validate(self):
+		if(self.loan_id != ' '):
+			if(self.loan_type=="Housing loan"):
+				loan_amount= self.loan_amount
+				interest_percent=self.interest_percent
+				max_loan_amount=1000000
+				min_loan_amount=10000
+				interest=self.interest
+				if(loan_amount >= min_loan_amount and loan_amount <= max_loan_amount):
 			#interest=loan_amount/10
 			#frappe.msgprint("Sucessfully Your loan is Applied !!")
-			if(loan_amount<=500000):
-				m_p=loan_amount*(5/100)
-				self.interest=(loan_amount+m_p)/(5*12)
-				self.interest=int(self.interest)
-				self.total_months=5*12
-				t_amount=loan_amount+m_p
-				self.total_amount=t_amount
-			else:
-				m_p=loan_amount*(10/100)
-				self.interest=(loan_amount+m_p)/(10*12)
-				self.total_months=10*12
-				t_amount=loan_amount+m_p
-				self.total_amount=t_amount
+					if(loan_amount<=500000):
+						self.total_months=60
+						t_amount=loan_amount+(loan_amount*(interest_percent/100))
+						self.total_amount=t_amount
+						mp=self.total_amount/60
+						self.interest=mp
+						
+					else:
+						self.total_months=120
+						t_amount=loan_amount+(loan_amount*(interest_percent/100))
+						self.total_amount=t_amount
+						mp=self.total_amount/120
+						self.interest=mp
 				
-		else:
-			frappe.throw("Loan amount is not in loan range")
+			if(self.loan_type=="Personal loan"):
+				loan_amount= self.loan_amount
+				interest_percent=self.interest_percent
+				max_loan_amount=1000000
+				min_loan_amount=10000
+				interest=self.interest
+				if(loan_amount >= min_loan_amount and loan_amount <= max_loan_amount):
+			#interest=loan_amount/10
+			#frappe.msgprint("Sucessfully Your loan is Applied !!")
+					if(loan_amount<=500000):
+						self.total_months=60
+						t_amount=loan_amount+(loan_amount*(interest_percent/100))
+						self.total_amount=t_amount
+						mp=self.total_amount/60
+						self.interest=mp
+						
+					else:
+						self.total_months=120
+						t_amount=loan_amount+(loan_amount*(interest_percent/100))
+						self.total_amount=t_amount
+						mp=self.total_amount/120
+						self.interest=mp
 
 
-    #def on_submit(self):
+			if(self.loan_type=="Jewel loan"):
+				loan_amount= self.loan_amount
+				interest_percent=self.interest_percent
+				max_loan_amount=1000000
+				min_loan_amount=10000
+				interest=self.interest
+				if(loan_amount >= min_loan_amount and loan_amount <= max_loan_amount):
+			#interest=loan_amount/10
+			#frappe.msgprint("Sucessfully Your loan is Applied !!")
+					if(loan_amount<=500000):
+						self.total_months=60
+						t_amount=loan_amount+(loan_amount*(interest_percent/100))
+						self.total_amount=t_amount
+						mp=self.total_amount/60
+						self.interest=mp
+						
+					else:
+						self.total_months=120
+						t_amount=loan_amount+(loan_amount*(interest_percent/100))
+						self.total_amount=t_amount
+						mp=self.total_amount/120
+						self.interest=mp
+
+			if(self.loan_type=="Buisness loan"):
+				loan_amount= self.loan_amount
+				interest_percent=self.interest_percent
+				max_loan_amount=1000000
+				min_loan_amount=10000
+				interest=self.interest
+				if(loan_amount >= min_loan_amount and loan_amount <= max_loan_amount):
+			#interest=loan_amount/10
+			#frappe.msgprint("Sucessfully Your loan is Applied !!")
+					if(loan_amount<=500000):
+						self.total_months=60
+						t_amount=loan_amount+(loan_amount*(interest_percent/100))
+						self.total_amount=t_amount
+						mp=self.total_amount/60
+						self.interest=mp
+						
+					else:
+						self.total_months=120
+						t_amount=loan_amount+(loan_amount*(interest_percent/100))
+						self.total_amount=t_amount
+						mp=self.total_amount/120
+						self.interest=mp
+
+
+		    
+	def on_sumbit(self):
 		if frappe.db.exists("payslip",{'id':self.loan_id}):
 			pass
 		else:
+			#frappe.msgprint("hhj")
 			xy=frappe.new_doc("payslip")
 			xy.customer_name=self.customer_name
+			xy.loan_type=self.type_of_loan
 			xy.id=self.loan_id
 			xy.total_amount=self.total_amount
 			xy.monthly_pay=self.interest
